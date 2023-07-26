51ed0d1262fb1621cafca31b3e7ae14fd7128d3b
durgesh2216 <durgesh.bagade@decimalpointanalytics.com>
2023-07-26 15:58:30 +0530

    fixed tailwind

--- src/Example.js
+++ src/Example.js
@@ -1,13 +1,13 @@
 const Example = () => {
     return (
-        <div>
+        <div class="flex flex-col ">
             {/*top*/}
             <div>
-
+                <h1 class="text-center text-blue-900">GPT 3.5</h1>
             </div>
             {/*heading*/}
-            <div class="text-center" >
-                <h1>dpaGPT</h1>
+            <div  >
+                <h1 class="text-center ">dpaGPT</h1>
             </div>
             {/*examples*/}
             <div>
