132b5dfe5b3543635864b02293d94b9c3e6205d3
pushpendra <pushpendra.vishwakarma@decimalpointanalytics.com>
2023-07-26 15:46:19 +0530

    Prompt Part Done

--- src/Prompt.js
+++ src/Prompt.js
@@ -1,4 +1,17 @@
-const Prompt = ()=>{
-    return <div>Prompt</div>
+import "./prompt.css"
+import MicSVG from "./assets/mic.svg"
+import SendSVG from "./assets/send.svg"
+const Prompt = () => {
+    return <div className="main-prompt">
+        <div>
+            <input type="text" placeholder="Please Type here your prompt..." />
+            <div className="mic">
+                <img src={SendSVG} width={"24px"} />
+            </div>
+            <div className="mic">
+                <img src={MicSVG} width={"24px"} />
+            </div>
+        </div>
+    </div>
 }
 export default Prompt;
