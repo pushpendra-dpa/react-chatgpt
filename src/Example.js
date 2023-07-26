442930e7113f58a9bd356f9c8dbc4661cda51836
durgesh2216 <durgesh.bagade@decimalpointanalytics.com>
2023-07-26 15:27:23 +0530

    example distribution

--- src/Example.js
+++ src/Example.js
@@ -1,7 +1,20 @@
-const Example = ()=> {
-    return(
+const Example = () => {
+    return (
         <div>
-            
+            {/*top*/}
+            <div>
+
+            </div>
+            {/*heading*/}
+            <div class="text-center" >
+                <h1>dpaGPT</h1>
+            </div>
+            {/*examples*/}
+            <div>
+
+
+            </div>
+
         </div>
     )
 }
