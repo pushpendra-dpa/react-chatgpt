48f829ff050cb0ad764c096c10722fbc0d5a80c2
durgesh2216 <durgesh.bagade@decimalpointanalytics.com>
2023-07-27 09:50:20 +0530

    Example List

--- src/Example.js
+++ src/Example.js
@@ -1,17 +1,19 @@
+import ExampleList from "./examplelist";
 const Example = () => {
     return (
         <div class="flex flex-col ">
             {/*top*/}
             <div>
-                <h1 class="text-center text-blue-900">GPT 3.5</h1>
+
+                <h1 class="text-center text-white pt-5 text-2xl">GPT 3.5</h1>
             </div>
             {/*heading*/}
             <div  >
-                <h1 class="text-center ">dpaGPT</h1>
+                <h1 class="text-center font-bold text-4xl pt-20 ">dpaGPT</h1>
             </div>
             {/*examples*/}
             <div>
-
+                <ExampleList />
 
             </div>
 
